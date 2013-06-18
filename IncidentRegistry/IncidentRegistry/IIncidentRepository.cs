b73cfe7ed65903364728990bd744e27f2b63978d
Priyanka <priyanka.lalchandani@gmail.com>
2013-06-18 17:10:40 +0530

    RepositoryClass Creation

---
+++ IncidentRegistry/IncidentRegistry/IIncidentRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using IncidentRegistry.Models;
+
+namespace IncidentRegistry
+{
+    public class IIncidentRepository:IDisposable
+    {
+        IEnumerable<Incident> GetIncidents();
+        Incident GetIncidentByID(int incidentId);
+        void InsertIncident(Incident incident);
+    }
+}
---
+++ IncidentRegistry/IncidentRegistry/IncidentRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using IncidentRegistry.Models;
+
+namespace IncidentRegistry
+{
+    public class IncidentRepository:IIncidentRepository,IDisposable
+    {
+        private IncidentDBContext context;
+
+        public IncidentRepository(IncidentDBContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<Incident> GetIncidents()
+        {
+            return context.incident.ToList();
+        }
+
+        public Incident GetIncidentByID(int incidentID)
+        {
+            return context.incident.Find(incidentID);
+        }
+
+        public void InsertIncident(Incident incident)
+        {
+            context.incident.Add(incident);
+        }
+    }
+}
