740cdf5e9ceadfba48eb6481ce824e6797daefe4
Priyanka <priyanka.lalchandani@gmail.com>
2013-06-18 11:30:53 +0530

    IncidentController and Views Creation

---
+++ IncidentRegistry/IncidentRegistry/Controllers/IncidentController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IncidentRegistry.Models;
+
+namespace IncidentRegistry.Controllers
+{
+    public class IncidentController : Controller
+    {
+        private IncidentDBContext db = new IncidentDBContext();
+
+        //
+        // GET: /Incident/
+
+        public ActionResult Index()
+        {
+            var incident = db.incident.Include(i => i.Employee);
+            return View(incident.ToList());
+        }
+
+        //
+        // GET: /Incident/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Incident incident = db.incident.Find(id);
+            if (incident == null)
+            {
+                return HttpNotFound();
+            }
+            return View(incident);
+        }
+
+        //
+        // GET: /Incident/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.EmployeeID = new SelectList(db.Employees, "EmployeeID", "EmployeeName");
+            return View();
+        }
+
+        //
+        // POST: /Incident/Create
+
+        [HttpPost]
+        public ActionResult Create(Incident incident)
+        {
+            if (ModelState.IsValid)
+            {
+                db.incident.Add(incident);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.EmployeeID = new SelectList(db.Employees, "EmployeeID", "EmployeeName", incident.EmployeeID);
+            return View(incident);
+        }
+
+        //
+        // GET: /Incident/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Incident incident = db.incident.Find(id);
+            if (incident == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.EmployeeID = new SelectList(db.Employees, "EmployeeID", "EmployeeName", incident.EmployeeID);
+            return View(incident);
+        }
+
+        //
+        // POST: /Incident/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Incident incident)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(incident).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.EmployeeID = new SelectList(db.Employees, "EmployeeID", "EmployeeName", incident.EmployeeID);
+            return View(incident);
+        }
+
+        //
+        // GET: /Incident/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Incident incident = db.incident.Find(id);
+            if (incident == null)
+            {
+                return HttpNotFound();
+            }
+            return View(incident);
+        }
+
+        //
+        // POST: /Incident/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Incident incident = db.incident.Find(id);
+            db.incident.Remove(incident);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
