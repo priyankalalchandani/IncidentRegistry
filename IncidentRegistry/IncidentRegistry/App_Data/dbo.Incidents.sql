8637ea2ff7a0b65d99337f9d9c52ad4d8a1a7fe7
Priyanka <priyanka.lalchandani@gmail.com>
2013-06-18 12:44:20 +0530

    DatabseUpdation
    Entered data for employee in Employee table so that it can be linked as
    a foreign key to Incident table

---
+++ IncidentRegistry/IncidentRegistry/App_Data/dbo.Incidents.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[Incidents] (
+    [IncidentID]       INT NOT NULL,
+    [Date]             DATETIME       NOT NULL,
+    [IncidentType]     NVARCHAR (MAX) NULL,
+    [Department]       NVARCHAR (MAX) NULL,
+    [Description]      NVARCHAR (MAX) NULL,
+    [IncidentLocation] NVARCHAR (MAX) NULL,
+    [ActionTaken]      NVARCHAR (MAX) NULL,
+    [EmployeeID]       INT            NOT NULL,
+    CONSTRAINT [PK_dbo.Incidents] PRIMARY KEY CLUSTERED ([IncidentID] ASC),
+    CONSTRAINT [FK_dbo.Incidents_dbo.Employees_EmployeeID] FOREIGN KEY ([EmployeeID]) REFERENCES [dbo].[Employees] ([EmployeeID]) ON DELETE CASCADE
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_EmployeeID]
+    ON [dbo].[Incidents]([EmployeeID] ASC);
+
