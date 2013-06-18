5a2d883e0cd204a88111c8cbb444db2dd8db9e3a
Priyanka <priyanka.lalchandani@gmail.com>
2013-06-18 10:58:07 +0530

    DbContext Creation

--- IncidentRegistry/IncidentRegistry/Models/Incident.cs
+++ IncidentRegistry/IncidentRegistry/Models/Incident.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
 
 namespace IncidentRegistry.Models
 {
@@ -18,4 +19,9 @@ namespace IncidentRegistry.Models
 
         public virtual Employee Employee { get; set; }
      }
+
+    public class IncidentDBContext : DbContext
+    {
+        public DbSet<Incident> incident { get; set; }
+    }
 }
