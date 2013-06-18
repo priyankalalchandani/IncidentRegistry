c8317a5720f6461c7e05e5e3140be643c7379089
Priyanka <priyanka.lalchandani@gmail.com>
2013-06-18 10:42:03 +0530

    IncidentModel Cration

---
+++ IncidentRegistry/IncidentRegistry/Models/Incident.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IncidentRegistry.Models
+{
+    public class Incident
+    {
+        public int IncidentID { get; set; }
+        public DateTime Date { get; set; }
+        public string IncidentType { get; set; }
+        public string Department { get; set; }
+        public string Description { get; set; }
+        public string IncidentLocation { get; set; }
+        public string ActionTaken { get; set; }
+        public int EmployeeID { get; set; }
+
+        public virtual Employee Employee { get; set; }
+     }
+}
