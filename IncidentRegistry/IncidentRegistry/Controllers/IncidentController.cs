af2135ed9602cf8bd5c1c00340aa8dea83cdfcde
Priyanka <priyanka.lalchandani@gmail.com>
2013-06-18 18:25:25 +0530

    Incident Controller(updated)

--- IncidentRegistry/IncidentRegistry/Controllers/IncidentController.cs
+++ IncidentRegistry/IncidentRegistry/Controllers/IncidentController.cs
@@ -11,15 +11,34 @@ namespace IncidentRegistry.Controllers
 {
     public class IncidentController : Controller
     {
-        private IncidentDBContext db = new IncidentDBContext();
+        //private IncidentDBContext db = new IncidentDBContext();
+        private IIncidentRepository incidentRepository;
+        private IEmployeeRepository employeeRepository;
+
         int i = 100;
+
+        public IncidentController()
+        {
+            this.incidentRepository = new IncidentRepository(new IncidentDBContext());
+            this.employeeRepository = new EmployeeRepository(new IncidentDBContext());
+        }
+
+        public IncidentController(IIncidentRepository incidentRepository)
+        {
+            this.incidentRepository = incidentRepository;
+            
+        }
+
+        
         //
         // GET: /Incident/
 
         public ActionResult Index()
         {
-            var incident = db.incident.Include(i => i.Employee);
-            return View(incident.ToList());
+            /*var incident = db.incident.Include(i => i.Employee);
+            return View(incident.ToList());*/
+
+            return View(incidentRepository.GetIncidents());
         }
 
         //
@@ -27,22 +46,22 @@ namespace IncidentRegistry.Controllers
 
         public ActionResult Details(int id = 0)
         {
-            Incident incident = db.incident.Find(id);
+            /*Incident incident = db.incident.Find(id);
             if (incident == null)
             {
                 return HttpNotFound();
             }
+            return View(incident);*/
+
+            Incident incident = incidentRepository.GetIncidentByID(id);
             return View(incident);
         }
 
         public ActionResult EmployeeDetails(int id = 0)
         {
-            Employee emp = db.Employees.Find(id);
-            if (emp == null)
-            {
-                return HttpNotFound();
-            }
+            Employee emp = incidentRepository.GetEmployeeByID(id);
             return View(emp);
+          
         }
 
 
@@ -51,7 +70,7 @@ namespace IncidentRegistry.Controllers
 
         public ActionResult Create()
         {
-            ViewBag.EmployeeID = new SelectList(db.Employees, "EmployeeID", "EmployeeName");
+            ViewBag.EmployeeID = new SelectList(employeeRepository.GetEmployees(), "EmployeeID", "EmployeeID");
             return View();
         }
 
@@ -63,20 +82,19 @@ namespace IncidentRegistry.Controllers
         {
             if (ModelState.IsValid)
             {
-                incident.IncidentID = i++;
-                db.incident.Add(incident);
-                db.SaveChanges();
+                incidentRepository.InsertIncident(incident);
+                incidentRepository.Save();
                 return RedirectToAction("Index");
             }
 
-            ViewBag.EmployeeID = new SelectList(db.Employees, "EmployeeID", "EmployeeName", incident.EmployeeID);
+            ViewBag.EmployeeID = new SelectList(employeeRepository.GetEmployees(), "EmployeeID", "EmployeeID", incident.EmployeeID);
             return View(incident);
         }
 
         //
         // GET: /Incident/Edit/5
 
-        public ActionResult Edit(int id = 0)
+        /*public ActionResult Edit(int id = 0)
         {
             Incident incident = db.incident.Find(id);
             if (incident == null)
@@ -85,12 +103,12 @@ namespace IncidentRegistry.Controllers
             }
             ViewBag.EmployeeID = new SelectList(db.Employees, "EmployeeID", "EmployeeName", incident.EmployeeID);
             return View(incident);
-        }
+        }*/
 
         //
         // POST: /Incident/Edit/5
 
-        [HttpPost]
+        /*[HttpPost]
         public ActionResult Edit(Incident incident)
         {
             if (ModelState.IsValid)
@@ -101,12 +119,12 @@ namespace IncidentRegistry.Controllers
             }
             ViewBag.EmployeeID = new SelectList(db.Employees, "EmployeeID", "EmployeeName", incident.EmployeeID);
             return View(incident);
-        }
+        }*/
 
         //
         // GET: /Incident/Delete/5
 
-        public ActionResult Delete(int id = 0)
+        /*public ActionResult Delete(int id = 0)
         {
             Incident incident = db.incident.Find(id);
             if (incident == null)
@@ -114,23 +132,23 @@ namespace IncidentRegistry.Controllers
                 return HttpNotFound();
             }
             return View(incident);
-        }
+        }*/
 
         //
         // POST: /Incident/Delete/5
 
-        [HttpPost, ActionName("Delete")]
+        /*[HttpPost, ActionName("Delete")]
         public ActionResult DeleteConfirmed(int id)
         {
             Incident incident = db.incident.Find(id);
             db.incident.Remove(incident);
             db.SaveChanges();
             return RedirectToAction("Index");
-        }
+        }*/
 
         protected override void Dispose(bool disposing)
         {
-            db.Dispose();
+            incidentRepository.Dispose();
             base.Dispose(disposing);
         }
     }
