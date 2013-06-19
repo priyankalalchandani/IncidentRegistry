f1ec8cb30b218d12f29290d1e06de3e9ffb5a51d
Priyanka <priyanka.lalchandani@gmail.com>
2013-06-19 18:49:08 +0530

    Updated IncidentModel

--- IncidentRegistry/IncidentRegistry/Models/Incident.cs
+++ IncidentRegistry/IncidentRegistry/Models/Incident.cs
@@ -3,16 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
 
 namespace IncidentRegistry.Models
 {
     public class Incident
     {
+        [Key]
         public int IncidentID { get; set; }
         public DateTime Date { get; set; }
         public string IncidentType { get; set; }
         public string Department { get; set; }
+        
         public string Description { get; set; }
+        public string UploadFile { get; set; }
         public string IncidentLocation { get; set; }
         public string ActionTaken { get; set; }
         public int EmployeeID { get; set; }
@@ -23,5 +27,7 @@ namespace IncidentRegistry.Models
     public class IncidentDBContext : DbContext
     {
         public DbSet<Incident> incident { get; set; }
+
+        public DbSet<Employee> Employees { get; set; }
     }
 }
