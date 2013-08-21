e8184588d36d871de5b91224f6d0f63398efd12f
unknown <priyanka.lalchandani@gmail.com>
2013-08-20 18:57:08 -0700

    add abstract test methods

---
+++ MapperTestProject/AbstractTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading;
+using System.Security.Principal;
+
+namespace MapperTestProject
+{
+    [TestClass]
+    public class AbstractTest<T> where T:class
+    {
+        protected T Service { get; set; }
+
+        public AbstractTest()
+        {
+            Initialize();
+        }
+
+        protected virtual void Initialize()
+        {
+            Service = Bootstrapper.Resolve<T>();    
+        }
+
+        protected virtual void Reset()
+        {
+            Bootstrapper.Reset();
+            Initialize();
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            Thread.CurrentPrincipal = new WindowsPrincipal(WindowsIdentity.GetCurrent());
+        }
+    }
+}
---
+++ MapperTestProject/Bootstrapper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Unity;
+using IncidentDomain.Services;
+using Microsoft.Practices.ServiceLocation;
+using IncidentInfrastructure.Repositories;
+using IncidentDomain.Repository;
+using IncidentRegistry;
+using System.Data.Entity;
+using IncidentDomain;
+
+namespace MapperTestProject
+{
+    public static class Bootstrapper
+    {
+        private static IUnityContainer Container { get; set; }
+
+        static Bootstrapper()
+        {
+            Initialize();
+        }
+
+        private static void Initialize()
+        {
+            Container = BuildUnityContainer();
+            ServiceLocator.SetLocatorProvider(() => new UnityServiceLocator(Container));
+        }
+
+        public static void Reset()
+        {
+            if (Container != null)
+            {
+                Container.Dispose();
+            }
+
+            Initialize();
+        }
+
+        public static T Resolve<T>()
+        {
+            return Container.Resolve<T>();
+        }
+
+        private static IUnityContainer BuildUnityContainer()
+        {
+            var container = new UnityContainer();
+
+            container.RegisterType<IIncidentService, IncidentService>();
+            container.RegisterType<IIncidentRepository, IncidentRepository>();
+
+            container.RegisterType<IncidentInfrastructure.IncidentDbContext>(new HierarchicalLifetimeManager());
+            container.RegisterType<DbContext, IncidentInfrastructure.IncidentDbContext>(new HierarchicalLifetimeManager());
+            container.RegisterType<ICommit, IncidentInfrastructure.IncidentDbContext>();
+
+            return container;
+        }
+    }
+}
