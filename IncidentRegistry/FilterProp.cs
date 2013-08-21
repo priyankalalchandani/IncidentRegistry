f8c5047aac6fac20d0a6c30e3c9cf435b9edaf29
unknown <priyanka.lalchandani@gmail.com>
2013-08-20 18:51:21 -0700

    add mapping for viewmodel and entity

---
+++ IncidentRegistry/FilterProp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Omu.ValueInjecter;
+
+namespace IncidentRegistry
+{
+    public class FilterProp : LoopValueInjection
+    {
+        int i;
+        protected override bool UseSourceProp(string sourcePropName)
+        {
+            i = 1;
+
+            if (i == 1)
+                return sourcePropName != "IncidentName" && sourcePropName != "Description";
+            else
+                return sourcePropName != "IncidentLocation";
+        }
+    }
+}
